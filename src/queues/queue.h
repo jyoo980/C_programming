15663ab89e147f70b7016ada4bc61cc461c259de
James Yoo <jyoo980@gmail.com>
2017-10-22 10:07:29 -0700

    Added stack

---
+++ src/queues/queue.h
@@ -0,0 +1,10 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+typedef struct queue {
+  int* items
+  int front, int rear;
+  int max_capacity;
+} queue;
+
+queue* create(int* num_items);
--- src/trees/binary_tree.h
+++
@@ -1,9 +0,0 @@
-#include<stdlib.h>
-
-typedef struct node {
-  int data;
-  struct node* left;
-  struct node* right;
-} node;
-
-node* create_root(int data); 
