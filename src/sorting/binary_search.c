b375e8d1c3d2dd23742c53ec65153e900c4cd32a
James Yoo <jyoo@appneta.com>
2017-10-25 09:32:48 -0700

    Completed Binary Search

--- src/sorting/binary_search.c
+++ src/sorting/binary_search.c
@@ -14,3 +14,19 @@ int binary_search(int* array, int target, int nums)
 {
   return search_helper(array, target, 0, nums - 1);
 }
+
+int search_helper(int* array, int target, start, end)
+{
+  int mid = start + (end - start) / 2;
+
+  if (start > end) {
+    printf("Element not found.\n")l
+    return -1;
+  } else if (array[mid] == target) {
+    return mid;
+  } else if (array[mid] > target) {
+    return search_helper(data, target, start, mid - 1);
+  } else {
+    return search_helper(data, target, mid + 1, end);
+  }
+}
